35c265b9fe4fe9572ad07416888a667903823659
phuparu <phumiphat.140@gmail.com>
2024-10-27 16:31:14 +0700

    About
    exclude brain svg

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
+import { Brain } from "../components/brain.jsx";
 
 const AboutPage = () => {
   return (
@@ -9,7 +10,200 @@ const AboutPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      About{" "}
+      {/* Container */}
+      <div className="h-full overflow-scroll">
+        {/* Text */}
+        <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+          {/* Bio */}
+          <div className="flex flex-col gap-12 justify-center">
+            <h1 className="text-2xl font-bold">BIOGRAPHY</h1>
+            <p className="text-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis quisquam molestiae placeat aut enim deleniti aliquid
+              illo sunt excepturi, dicta quibusdam sit, est mollitia culpa alias
+              sequi fugit nam earum?
+            </p>
+            <span className="italic">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </span>
+            {/* Signature */}
+            <div className="self-end">
+              <svg
+                width="242"
+                height="205"
+                viewBox="0 0 242 205"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 203C2.38407 198.775 4.31481 194.351 5.55556 190.333C12.3458 168.346 24.4071 147.694 36.2778 128.167C60.3182 88.621 89.2674 50.4905 122.667 18.3333C128.584 12.6363 135.264 7.06414 142.278 2.72222C147.281 -0.374977 150.491 5.21123 151.556 9.27778C154.9 22.049 156.142 37.8981 150.778 50.2778C145.074 63.4401 136.689 76.7749 125.556 86C120.336 90.3245 112.348 97 105 97C99.3317 97 105.738 84.0804 106.778 82C111.944 71.6684 120.084 66.7909 129.778 61.2222C137.762 56.6356 155 54.9542 155 67.5C155 74.6159 154.376 80.2121 152 87C150.11 92.4003 148.262 97.9339 145.556 103C142.762 108.229 138.16 112.629 134.944 117.667C127.965 128.6 121.099 139.962 115.056 151.444C112.81 155.711 111.29 160.108 108.056 163.611C106.111 165.718 105.274 169.401 103.556 171.611C102.21 173.341 98.6549 177.458 98.0556 179.556C97.4285 181.75 94.9823 185.495 93 186.778C89.9888 188.726 94.139 180.639 94.5556 179.667C99.958 167.061 109.473 154.02 119.556 144.778C125.134 139.664 132.426 131.717 140.667 131C145.441 130.585 162.834 121.536 165.5 129C167.281 133.986 167.76 140.828 166.889 146.056C166.073 150.949 163.142 154.83 163 160C162.882 164.305 163.101 168.297 161.5 172.389C159.246 178.149 159.393 181.479 165 184.778C171.591 188.655 180.009 183.777 187 183C188.632 182.819 192.744 177.34 193.778 176C198.014 170.509 201.336 164.074 204.222 157.778C206.614 152.56 211.681 144.747 212 139C212.123 136.794 213.793 134 216 134"
+                  stroke="black"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M208 95C208 92.4906 209.981 93 212 93"
+                  stroke="black"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M230 92C231.948 92.2164 233 93.1257 233 95"
+                  stroke="black"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M214 112C214.586 117.858 226.185 116.91 229.778 116.222C235.687 115.091 236.814 114.779 240 110"
+                  stroke="black"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </div>
+          </div>
+
+          {/* Skills */}
+          <div className="flex flex-col gap-12 justify-center">
+            {/* Title */}
+            <h1 className="text-2xl font-bold">SKILL</h1>
+            {/* List */}
+            <div className="flex gap-4 flex-wrap">
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black transition-all duration-300">
+                JavaScript
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black transition-all duration-300">
+                HTML
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black transition-all duration-300">
+                CSS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black transition-all duration-300">
+                React
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black transition-all duration-300">
+                TailwindCSS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black transition-all duration-300">
+                Python
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black transition-all duration-300">
+                NextJS
+              </div>
+            </div>
+            {/* Skill SCROLL SVG */}
+            <div className="w-full h-full">
+              <svg></svg>
+            </div>
+          </div>
+
+          {/* EXPERIENCE CONTAINER */}
+          <div className="flex flex-col gap-12 justify-center pb-48">
+            {/* EXPERIENCE TITLE */}
+            <h1 className="">EXPERIENCE</h1>
+            {/* EXPERIENCE LIST */}
+            <div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I led web development, offering expertise in JavaScript
+                    frameworks.{" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 "></div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 "></div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior React Developer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I spearheaded React-based application development,
+                    leveraging advanced skills.{" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2019 - 2024{" "}
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Microsoft
+                  </div>
+                </div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Freelancer{" "}
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I provided web solutions, applying a range of technologies
+                    to address client requirements.{" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2010 - 2019{" "}
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* SVG CONTAINER */}
+        <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2"></div>
+      </div>
     </motion.div>
   );
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import TransitionProvider from "./components/transitionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Lama Dev Portfolio App",
+  title: "My Portfolio App",
   description: "Animated portfoilio",
 };
 
